7258006b3823d8c800f59fcb3d15fc773f72902d
R2covery <r2covery@outlook.com>
2024-09-17 15:09:55 +0800

    Site updated: 2024-09-17 15:09:54

---
+++ js/fireworks.min.js
@@ -0,0 +1,6 @@
+<canvas
+    id="fireworks"
+    style="position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; pointer-events: none; z-index: 32767"
+></canvas>
+<script src="https://s4.zstatic.net/ajax/libs/animejs/3.2.1/anime.min.js"></script>
+<script src="/js/fireworks.min.js"></script>
